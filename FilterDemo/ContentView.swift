55880cae8fe742c2ffc14015066b6d9bfe4b1537
ShunKetsu Cho <j33852@gmail.com>
2024-10-11 11:51:58 +0900

    Initial Commit

---
+++ FilterDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FilterDemo
+//
+//  Created by Shunketsu Cho on 2024/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FilterDemo/FilterDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilterDemoApp.swift
+//  FilterDemo
+//
+//  Created by Shunketsu Cho on 2024/10/11.
+//
+
+import SwiftUI
+
+@main
+struct FilterDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
